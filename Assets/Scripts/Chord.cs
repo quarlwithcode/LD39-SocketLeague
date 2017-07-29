c1b42662e5ec39e8bd56cdd439fa24085a8c918c
MikeGOfficial <mikegofficial@gmail.com>
2017-07-29 17:11:50 -0400

    Messed with animating the chord.

---
+++ Assets/Scripts/Chord.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Chord : MonoBehaviour
+{
+    #region Variables
+    private LineRenderer lr;
+    [SerializeField] private GameObject player;
+    #endregion
+
+    #region MonoBehaviors
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        transform.LookAt(player.transform);
+        lr.SetPosition(0, transform.position);
+        lr.SetPosition(1, player.transform.position);
+    }
+    #endregion
+}
